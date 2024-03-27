02eaa10c1fe6ba4f0568b0344fc4034657cfb6de
Sumit Mahat <mahatsumit5@gmail.com>
2024-03-27 16:09:30 +1100

    Flatlist for ui performance when using dynamic data

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputBase,
+  View,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [enteredGoals, setEnteredGoals] = useState([]);
+  function goalInputHandler(text) {
+    setGoal(text);
+  }
+  function addGoalHandler() {
+    setEnteredGoals([
+      ...enteredGoals,
+      { text: goal, id: Math.random().toString() },
+    ]);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your Course Goal"
+          style={styles.textInput}
+          textContentType="familyName"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goals" onPress={addGoalHandler} />
+      </View>
+      <Text style={{ fontSize: 24 }}>List of Goals........</Text>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={enteredGoals}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={true}
+          renderItem={(item) => {
+            return (
+              <View style={styles.goalsText}>
+                <Text style={{ color: "white" }}>{item.item.text}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 70,
+    padding: 10,
+    gap: 20,
+
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    flex: 1,
+    gap: 5,
     alignItems: "center",
-    justifyContent: "center",
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderColor: "#d6c8c88f",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#bdb4b48f",
+    flex: 1,
+    height: 45,
+    padding: 10,
+    margin: "auto",
+    borderRadius: 5,
+  },
+  goalsContainer: {
+    flex: 10,
+  },
+  goalsText: {
+    padding: 15,
+    fontSize: 18,
+    backgroundColor: "#266ae9",
+    width: "auto",
+    textAlign: "left",
+    borderRadius: 10,
+    shadowRadius: 1,
+    shadowColor: "black",
+    elevation: 3,
+    gap: 10,
+    marginTop: 10,
   },
 });
